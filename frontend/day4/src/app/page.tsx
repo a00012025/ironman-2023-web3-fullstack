cb3687398ccd860e83aa4e2fb7e5caf8c63bfdf7
Harry Chen <a00012025@gmail.com>
2023-08-30 09:43:35 +0800

    feat: day4 add wagmi

--- frontend/day4/src/app/page.tsx
+++ frontend/day4/src/app/page.tsx
@@ -1,11 +1,49 @@
-import Image from "next/image";
+"use client";
+
+import {
+  WagmiConfig,
+  configureChains,
+  createConfig,
+  mainnet,
+  sepolia,
+} from "wagmi";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, sepolia],
+  [publicProvider()]
+);
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient: publicClient,
+});
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        Connect Wallet
-      </div>
-    </main>
+    <WagmiConfig config={config}>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Profile />
+      </main>
+    </WagmiConfig>
   );
+}
+
+function Profile() {
+  const { address, isConnected } = useAccount();
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  });
+  const { disconnect } = useDisconnect();
+
+  if (isConnected)
+    return (
+      <div>
+        Connected to {address}
+        <button onClick={() => disconnect()}>Disconnect</button>
+      </div>
+    );
+  return <button onClick={() => connect()}>Connect Wallet</button>;
 }
