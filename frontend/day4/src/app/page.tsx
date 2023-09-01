1c1ae8a9c081086a93d9d39a55670255a7e686ac
Harry Chen <a00012025@gmail.com>
2023-09-01 17:52:33 +0800

    feat: finish day 4

--- frontend/day4/src/app/page.tsx
+++ frontend/day4/src/app/page.tsx
@@ -9,13 +9,20 @@ import {
   configureChains,
   mainnet,
   sepolia,
+  useBalance,
+  useSwitchNetwork,
+  useNetwork,
 } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
+import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet, sepolia],
-  [publicProvider()]
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY! }),
+    publicProvider(),
+  ]
 );
 
 const config = createConfig({
@@ -29,12 +36,30 @@ function Profile() {
     connector: new InjectedConnector(),
   });
   const { disconnect } = useDisconnect();
+  const balance = useBalance({ address });
+
+  const { chain } = useNetwork();
+  const { error, switchNetwork } = useSwitchNetwork();
 
   if (isConnected)
     return (
       <div>
         <div>Connected to {address}</div>
+        <div>Balance: {balance.data?.formatted}</div>
         <button onClick={() => disconnect()}>Disconnect</button>
+
+        <div>Chains:</div>
+        {chain && <div>Connected to {chain.name}</div>}
+        {chains.map((x) => (
+          <div key={x.id}>
+            <button
+              disabled={!switchNetwork || x.id === chain?.id}
+              onClick={() => switchNetwork?.(x.id)}
+            >
+              {x.name} {x.id === chain?.id && "(current)"}
+            </button>
+          </div>
+        ))}
       </div>
     );
   return <button onClick={() => connect()}>Connect Wallet</button>;
