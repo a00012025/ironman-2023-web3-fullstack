3dff4b207ac0c0c9480e4ec651d010066f8ab134
Harry Chen <a00012025@gmail.com>
2023-09-02 15:58:43 +0800

    feat: finish day5

--- frontend/day5/src/app/page.tsx
+++ frontend/day5/src/app/page.tsx
@@ -12,7 +12,10 @@ import {
   useBalance,
   useSwitchNetwork,
   useNetwork,
+  useContractRead,
+  useContractWrite,
 } from "wagmi";
+import { formatUnits } from "viem";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
@@ -30,6 +33,68 @@ const config = createConfig({
   publicClient: publicClient,
 });
 
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "recipient",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "success",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+] as const;
+
+const UNI_CONTRACT_ADDRESS = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
+const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
+
 function Profile() {
   const { address, isConnected } = useAccount();
   const { connect } = useConnect({
@@ -39,7 +104,35 @@ function Profile() {
   const balance = useBalance({ address });
 
   const { chain } = useNetwork();
-  const { error, switchNetwork } = useSwitchNetwork();
+  const { switchNetwork } = useSwitchNetwork();
+
+  // read UNI balance
+  const { data: balanceData } = useContractRead({
+    address: UNI_CONTRACT_ADDRESS,
+    abi,
+    functionName: "balanceOf",
+    args: [address || NULL_ADDRESS],
+  });
+  const { data: decimals } = useContractRead({
+    address: UNI_CONTRACT_ADDRESS,
+    abi,
+    functionName: "decimals",
+  });
+  const uniBalance =
+    balanceData && decimals ? formatUnits(balanceData, decimals) : undefined;
+
+  // send UNI tx
+  const {
+    data: txData,
+    isLoading,
+    isSuccess,
+    write: sendUniTx,
+  } = useContractWrite({
+    address: UNI_CONTRACT_ADDRESS,
+    abi,
+    functionName: "transfer",
+    args: ["0xE2Dc3214f7096a94077E71A3E218243E289F1067", 100000n],
+  });
 
   if (isConnected)
     return (
@@ -60,9 +153,26 @@ function Profile() {
             </button>
           </div>
         ))}
+
+        {/* {balanceData !== undefined && <div>UNI Balance: {balanceData.toString()}</div>} */}
+        {uniBalance && (
+          <>
+            <div>UNI Balance: {uniBalance}</div>
+            <button onClick={() => sendUniTx()}>Send UNI</button>
+            {isLoading && <div>Check Your Wallet...</div>}
+            {isSuccess && <div>Transaction Hash: {txData?.hash}</div>}
+          </>
+        )}
       </div>
     );
   return <button onClick={() => connect()}>Connect Wallet</button>;
+}
+
+function Transaction() {
+  const { isConnected } = useAccount();
+
+  if (isConnected) return <div>123</div>;
+  return null;
 }
 
 export default function App() {
@@ -70,6 +180,7 @@ export default function App() {
     <WagmiConfig config={config}>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Profile />
+        <Transaction />
       </main>
     </WagmiConfig>
   );
