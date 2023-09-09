15d012e05260703427abca980b260f256088f3e0
Harry Chen <a00012025@gmail.com>
2023-09-09 16:14:04 +0800

    feat: day 13

---
+++ mobile/day13/lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:bip39/bip39.dart' as bip39;
+import 'package:flutter_bitcoin/flutter_bitcoin.dart' hide Transaction;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:web3dart/web3dart.dart';
+
+import 'tx.dart';
+
+void main() {
+  dotenv.load(fileName: '.env');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+const uniContractAddress = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController mnemonicController = TextEditingController();
+  String mnemonic = "";
+  HDWallet? ethWallet;
+  String? ethAddress;
+  BigInt? uniBalance;
+  String? ethTxHash;
+
+  void refreshMnemonic() {
+    final newMnemonic = mnemonicController.text;
+    if (bip39.validateMnemonic(newMnemonic)) {
+      final seed = bip39.mnemonicToSeed(newMnemonic);
+      final hdWallet = HDWallet.fromSeed(seed);
+      setState(() {
+        mnemonic = newMnemonic;
+        ethWallet = hdWallet.derivePath("m/44'/60'/0'/0/0");
+        final ethPriKey = EthPrivateKey.fromHex(ethWallet!.privKey!);
+        ethAddress = ethPriKey.address.hex;
+        readTokenBalance(uniContractAddress, ethAddress!).then((balance) {
+          setState(() {
+            uniBalance = balance;
+          });
+        });
+      });
+    } else {
+      setState(() {
+        ethWallet = null;
+        ethAddress = null;
+        uniBalance = null;
+        ethTxHash = null;
+      });
+    }
+  }
+
+  void sendToken() {
+    final ethPriKey = EthPrivateKey.fromHex(ethWallet!.privKey!);
+    sendTokenTransaction(
+      privateKey: ethPriKey,
+      contractAddress: uniContractAddress,
+      toAddress: "0xE2Dc3214f7096a94077E71A3E218243E289F1067",
+      amount: BigInt.from(10000),
+    ).then((txHash) {
+      setState(() {
+        ethTxHash = txHash;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    refreshMnemonic();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                controller: mnemonicController,
+                decoration: const InputDecoration(
+                  labelText: 'Enter mnemonic',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: 3,
+                onChanged: (text) {
+                  refreshMnemonic();
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            if (mnemonic.isNotEmpty) ...[
+              if (ethAddress != null) Text('Ethereum address: $ethAddress'),
+              const SizedBox(height: 10),
+              if (uniBalance != null) Text('UNI balance: $uniBalance'),
+              if (ethTxHash != null) Text('Ethereum tx hash: $ethTxHash'),
+              if (uniBalance != null)
+                SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: sendToken,
+                    child: const Text('Send Token Tx'),
+                  ),
+                )
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ mobile/day13/lib/tx.dart
@@ -0,0 +1,150 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:hex/hex.dart';
+import 'package:web3dart/web3dart.dart';
+import 'package:http/http.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+final alchemyApiKey = dotenv.get('ALCHEMY_API_KEY');
+final rpcUrl = 'https://eth-sepolia.g.alchemy.com/v2/$alchemyApiKey';
+final web3Client = Web3Client(rpcUrl, Client());
+
+Future<String> signTransaction({
+  required EthPrivateKey privateKey,
+  required Transaction transaction,
+}) async {
+  try {
+    final result = await web3Client.signTransaction(
+      privateKey,
+      transaction,
+      chainId: 11155111,
+    );
+    return HEX.encode(result);
+  } catch (e) {
+    rethrow;
+  }
+}
+
+Future<String> sendRawTransaction(String tx) async {
+  try {
+    final txHash =
+        await web3Client.sendRawTransaction(Uint8List.fromList(HEX.decode(tx)));
+    return txHash;
+  } catch (e) {
+    rethrow;
+  }
+}
+
+const abi = [
+  {
+    "inputs": [
+      {"internalType": "address", "name": "account", "type": "address"}
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {"internalType": "uint256", "name": "", "type": "uint256"}
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [
+      {"internalType": "uint8", "name": "", "type": "uint8"}
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {"internalType": "address", "name": "recipient", "type": "address"},
+      {"internalType": "uint256", "name": "amount", "type": "uint256"}
+    ],
+    "name": "transfer",
+    "outputs": [
+      {"internalType": "bool", "name": "", "type": "bool"}
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+];
+
+Future<BigInt> readTokenBalance(
+    String contractAddress, String walletAddress) async {
+  try {
+    final contract = DeployedContract(
+      ContractAbi.fromJson(
+        jsonEncode(abi),
+        'ERC20',
+      ),
+      EthereumAddress.fromHex(contractAddress),
+    );
+    final balanceFunction = contract.function('balanceOf');
+    final balance = await web3Client.call(
+      contract: contract,
+      function: balanceFunction,
+      params: [EthereumAddress.fromHex(walletAddress)],
+    );
+    final balanceStr = balance.first.toString();
+    return BigInt.parse(balanceStr);
+  } catch (e) {
+    rethrow;
+  }
+}
+
+Future<EtherAmount> getMaxPriorityFee() async {
+  try {
+    final response = await post(
+      Uri.parse(rpcUrl),
+      body: jsonEncode({
+        "jsonrpc": "2.0",
+        "method": "eth_maxPriorityFeePerGas",
+        "params": [],
+        "id": 1,
+      }),
+    );
+    final json = jsonDecode(response.body);
+    final result = json['result'];
+    return EtherAmount.fromBigInt(EtherUnit.wei, BigInt.parse(result));
+  } catch (e) {
+    rethrow;
+  }
+}
+
+Future<String> sendTokenTransaction({
+  required EthPrivateKey privateKey,
+  required String contractAddress,
+  required String toAddress,
+  required BigInt amount,
+}) async {
+  try {
+    final contract = DeployedContract(
+      ContractAbi.fromJson(
+        jsonEncode(abi),
+        'ERC20',
+      ),
+      EthereumAddress.fromHex(contractAddress),
+    );
+    final transferFunction = contract.function('transfer');
+    final transferTx = Transaction.callContract(
+      contract: contract,
+      function: transferFunction,
+      parameters: [EthereumAddress.fromHex(toAddress), amount],
+      gasPrice: await web3Client.getGasPrice(),
+      // If using EIP-1559, use the following instead of the above:
+      // maxFeePerGas: await web3Client.getGasPrice(),
+      // maxPriorityFeePerGas: await getMaxPriorityFee(),
+    );
+    final tx = await signTransaction(
+      privateKey: privateKey,
+      transaction: transferTx,
+    );
+    print('tx: $tx');
+    final txHash = await sendRawTransaction(tx);
+    print('txHash: $txHash');
+    return txHash;
+  } catch (e) {
+    rethrow;
+  }
+}
